17dd8b2228dcedc5fa4c3c9442a84eb38cbe557f
aminyeeet-outliant <abby.orioque@outliant.com>
2025-04-10 21:45:58 +0800

    fix failing scripts

--- tests/booking.spec.js
+++ tests/booking.spec.js
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('Site address should be similar to address in booking page', async ({ page }) => {
-  await page.goto('https://www.sit.orangetheory.com/en-us/locations/online-join-sit-7');
+  await page.goto('https://www.sit.orangetheory.com/en-us/locations/online-join-sit-5');
 
   // Get the address line on home page
   const addressLine1 = await page.locator("div.active .studio-physical-address").innerText()
   const addressLine2 = await page.locator("div.active .studio-physical-address + div").innerText()
   const fullAddress = `${addressLine1} ${addressLine2}`
-  await page.locator("#try-class-banner-mobile").click()
+  await page.locator("#try-class-navbar").click()
   
   const frame = await page.waitForEvent("frameattached")
   await frame.waitForSelector("#firstName", {timeout: 10000})
@@ -23,22 +23,22 @@ test('Site address should be similar to address in booking page', async ({ page 
 
 // Additional test case
 test('Phone number and Zip code must have input validation', async ({page}) => {
-  await page.goto('https://www.sit.orangetheory.com/en-us/locations/online-join-sit-7');
-  await page.locator("#try-class-banner-mobile").click()
+  await page.goto('https://www.sit.orangetheory.com/en-us/locations/online-join-sit-5');
+  await page.locator("#try-class-navbar").click()
   const frame = await page.waitForEvent("frameattached")
 
   // Input fields data
   await frame.waitForSelector("#firstName", {timeout: 10000})
-  await frame.locator("#firstName").fill("Test")
-  await frame.locator("#lastName").fill("Test")
-  await frame.locator("#email").fill("test@gmail.com")
-  await frame.locator("#phoneNum").fill("test")
-  await frame.locator("#zipCode").fill("test")
+  await frame.locator("#firstName input").fill("Test")
+  await frame.locator("#lastName input").fill("Test")
+  await frame.locator("#email input").fill("test@gmail.com")
+  await frame.locator("#phoneNum input").fill("test")
+  await frame.locator("#zipCode input").fill("test")
   await frame.locator("#isLocalResident").check()
   await frame.locator("button", {hasText: "Next"}).click()
 
   // Phone number must have validation
-  await expect(frame.locator("#phoneNum + div.invalid-feedback")).toBeVisible()
+  await expect(frame.locator("#phoneNum + span.text-error")).toBeVisible()
   // Zip code must have validation
-  await expect(frame.locator("#zipCode + div.invalid-feedback")).toBeVisible()
+  await expect(frame.locator("#zipCode + span.text-error")).toBeVisible()
 })
--- tests/generateLink.spec.js
+++ tests/generateLink.spec.js
@@ -13,6 +13,8 @@ test("Generate link", async ({ page }) => {
         route.continue()
     })
 
+    let locationId = "f627d35c-9e2b-452a-8017-bfbcccff5a4d"
+
     // Raw data that will be converted into base64
     const rawData = {
         "person_id": "otfqa319-aa48-42fa-8888-52ff2e914ea2199",
@@ -40,14 +42,15 @@ test("Generate link", async ({ page }) => {
             "value": "Papa Johns"
         },
         "check_id": true,
-        "contract_start_date": "2032-02-25T00:00:00"
+        "contract_start_date": "2032-02-25T00:00:00",
+        "location_id": locationId
     };
 
     // Convert raw data to base64
     const base64Data = Buffer.from(JSON.stringify(rawData)).toString('base64')
 
     // This is now the generate link
-    const generatedLink = `https://www.sit.orangetheory.com/en-us/membership-agreement?studioid=bf60d4c9-f9c3-4e5c-97f2-fe1118531493&data=${base64Data}`
+    const generatedLink = `https://www.sit.orangetheory.com/en-us/membership-agreement?location_id=${locationId}&data=${base64Data}`
     console.log(generatedLink)
     await page.goto(generatedLink)
     const res = await page.waitForResponse(validateURL)
@@ -57,6 +60,7 @@ test("Generate link", async ({ page }) => {
     expect(JSON.stringify(validateRequestPayload?.data)).toBe(JSON.stringify(rawData))
 
     // Validate submission
+    await page.frameLocator("#membership-agreement-iframe").locator("#cancelConsent").click()
     const confirmButton = page.frameLocator("#membership-agreement-iframe").getByText("Confirm")
     await confirmButton.click({delay: 5000})
     await expect(page.frameLocator("#membership-agreement-iframe").getByText("I agree", {exact: true})).toBeVisible()
